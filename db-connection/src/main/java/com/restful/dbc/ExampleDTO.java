a94a5eb48209004bf0bb2c791cc21982f407f2e0
남대영 <skaeodud0507@namdaeyeong-ui-MacBookPro.local>
2021-05-12 12:31:15 +0900

    module init

---
+++ db-connection/src/main/java/com/restful/dbc/ExampleDTO.java
@@ -0,0 +1,15 @@
+package com.restful.dbc;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ExampleDTO {
+
+    private String name;
+    private int age;
+
+}
